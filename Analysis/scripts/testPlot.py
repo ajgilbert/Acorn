464b3c11b12e93cf27a30e0218e6de8ce294dd52
Andrew Gilbert <andrew.gilbert@cern.ch>
2018-06-08 17:23:08 +0200

    more plotting tests

--- Analysis/scripts/testPlot.py
+++ Analysis/scripts/testPlot.py
@@ -104,38 +104,48 @@ GenTreeCode.counter = 0
 
 tname = 'DiMuonAnalysis'
 
-# samples = {
-#     'DY': 'output/020618/wgamma_2016_v1/Main/DYJetsToLL_M-50-madgraphMLM.root',
-#     'data_obs': 'output/020618/wgamma_2016_v1/Main/SingleMuon.root',
-#     'TT': 'output/020618/wgamma_2016_v1/Main/TT-powheg.root',
-#     'WG': 'output/020618/wgamma_2016_v1/Main/WGToLNuG-madgraphMLM.root'
-# }
-
-# remap = {
-#     'DY': 'DYJetsToLL_M-50-madgraphMLM',
-#     'data_obs': 'SingleMuon',
-#     'TT': 'TT-powheg',
-#     'WG': 'WGToLNuG-madgraphMLM'
-# }
-
 samples = {
-    'data_obs': 'output/020618/wgamma_2018_v1/Main/SingleMuon.root',
-    'DY': 'output/020618/wgamma_2018_v1/Main/DYJetsToLL_M-50-madgraphMLM.root',
-    'TT': 'output/020618/wgamma_2018_v1/Main/TTTo2L2Nu-powheg.root'
+    'DY': 'output/060618/wgamma_2016_v1/Main/DYJetsToLL_M-50-madgraphMLM.root',
+    'data_obs': 'output/060618/wgamma_2016_v1/Main/SingleMuon.root',
+    'TT': 'output/060618/wgamma_2016_v1/Main/TT-powheg.root',
+    'WG': 'output/060618/wgamma_2016_v1/Main/WGToLNuG-madgraphMLM.root'
 }
 
 remap = {
     'DY': 'DYJetsToLL_M-50-madgraphMLM',
     'data_obs': 'SingleMuon',
-    'TT': 'TTTo2L2Nu-powheg',
+    'TT': 'TT-powheg',
     'WG': 'WGToLNuG-madgraphMLM'
 }
 
+# samples = {
+#     'data_obs': 'output/020618/wgamma_2018_v1/Main/SingleMuon.root',
+#     'DY': 'output/020618/wgamma_2018_v1/Main/DYJetsToLL_M-50-madgraphMLM.root',
+#     'TT': 'output/020618/wgamma_2018_v1/Main/TTTo2L2Nu-powheg.root'
+# }
+
+# remap = {
+#     'DY': 'DYJetsToLL_M-50-madgraphMLM',
+#     'data_obs': 'SingleMuon',
+#     'TT': 'TTTo2L2Nu-powheg',
+#     'WG': 'WGToLNuG-madgraphMLM'
+# }
+
 hists = Node()
 
-for ptcut in range(30, 100, 5):
+for ptcut in range(30, 100, 10):
     for sample in samples:
-        hists['m_ll']['pt_%s' % ptcut][sample] = Define('TH1F', (60, 60., 120.), sample, ['m_ll'], sel='pt_1 > %s' % ptcut)
+        hists['m_ll']['pt_%s' % ptcut][sample] = Define('TH1F', (60, 60., 120.), sample, ['m_ll'], sel='pt_1 > %s && trg_1' % ptcut)
+        hists['eta_1']['pt_%s' % ptcut][sample] = Define('TH1F', (40, -2.4, 2.4), sample, ['eta_1'], sel='pt_1 > %s && trg_1' % ptcut)
+        hists['eta_2']['pt_%s' % ptcut][sample] = Define('TH1F', (40, -2.4, 2.4), sample, ['eta_2'], sel='pt_1 > %s && trg_1' % ptcut)
+        hists['pt_1']['pt_%s' % ptcut][sample] = Define('TH1F', (40, 0, 100), sample, ['pt_1'], sel='pt_1 > %s && trg_1' % ptcut)
+        hists['pt_2']['pt_%s' % ptcut][sample] = Define('TH1F', (40, 0, 100), sample, ['pt_2'], sel='pt_1 > %s && trg_1' % ptcut)
+    for sample in samples:
+        hists['m_ll_weighted']['pt_%s' % ptcut][sample] = Define('TH1F', (60, 60., 120.), sample, ['m_ll'], sel='pt_1 > %s && trg_1' % ptcut, wt='wt_pu*wt_1*wt_2*wt_trg1')
+        hists['eta_1_weighted']['pt_%s' % ptcut][sample] = Define('TH1F', (40, -2.4, 2.4), sample, ['eta_1'], sel='pt_1 > %s && trg_1' % ptcut, wt='wt_pu*wt_1*wt_2*wt_trg1')
+        hists['eta_2_weighted']['pt_%s' % ptcut][sample] = Define('TH1F', (40, -2.4, 2.4), sample, ['eta_2'], sel='pt_1 > %s && trg_1' % ptcut, wt='wt_pu*wt_1*wt_2*wt_trg1')
+        hists['pt_1_weighted']['pt_%s' % ptcut][sample] = Define('TH1F', (40, 0, 100), sample, ['pt_1'], sel='pt_1 > %s && trg_1' % ptcut, wt='wt_pu*wt_1*wt_2*wt_trg1')
+        hists['pt_2_weighted']['pt_%s' % ptcut][sample] = Define('TH1F', (40, 0, 100), sample, ['pt_2'], sel='pt_1 > %s && trg_1' % ptcut, wt='wt_pu*wt_1*wt_2*wt_trg1')
 
 
 drawtree = defaultdict(list)
@@ -143,7 +153,7 @@ for path, name, obj in hists.ListObjects():
     drawtree[obj.sample].append(obj)
 
 pprint(drawtree)
-with open('input/cfg_wgamma_2018_v1.json') as jsonfile:
+with open('input/cfg_wgamma_2016_v1.json') as jsonfile:
     cfg = json.load(jsonfile)
     sample_cfg = cfg['samples']
 
@@ -171,7 +181,7 @@ for path, name, obj in hists.ListObjects():
         scale = tgt_lumi * xsec / events
         obj.Scale(scale)
 
-fout = ROOT.TFile('output_2018.root', 'RECREATE')
+fout = ROOT.TFile('output_2016.root', 'RECREATE')
 
 for path, name, obj in hists.ListObjects():
     WriteToTFile(obj, fout, path, name)
