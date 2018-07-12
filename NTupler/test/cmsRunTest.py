6a1527b39ade681c2fd6aaf4b7faa6d6d9d8379e
Andrew Gilbert <andrew.gilbert@cern.ch>
2018-07-12 13:36:21 +0200

    adding cmsRunTest.py script

---
+++ NTupler/test/cmsRunTest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import ROOT
+import argparse
+import re
+import subprocess
+import sys
+import json
+
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(ROOT.kTRUE)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('config', default='config.json',
+                    help='Specifies the sample config json')
+parser.add_argument('--samples', '-s', default=None,
+                    help='Specifies the samples to run')
+
+args, passthru = parser.parse_known_args()
+
+
+with open(args.config) as jsonfile:
+    cfg = json.load(jsonfile)
+
+samples = []
+if args.samples is not None:
+    samples = args.samples.split(',')
+
+
+for s in samples:
+    info = cfg['samples'][s]
+    dataset = info['dataset']
+    runargs = ['cmsRun'] + passthru
+    runargs.extend(cfg['configs'][info['config']])
+    runargs.append('events=1000')
+    infile = subprocess.check_output(['dasgoclient', '-query', 'file dataset=%s' % dataset, '-limit', '1']).strip()
+    runargs.append('input=%s' % infile)
+    print ' '.join(runargs)
+    subprocess.call(runargs)
