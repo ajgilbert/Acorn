a8b5a95d498874ade4cccfae65999373f9742d92
Andrew Gilbert <andrew.gilbert@cern.ch>
2018-09-07 10:56:08 +0200

    genPlot.py: adding TGraphs of bin yields vs C3W

--- Analysis/scripts/genPlot.py
+++ Analysis/scripts/genPlot.py
@@ -2,6 +2,7 @@ import ROOT
 from pprint import pprint
 import CombineHarvester.CombineTools.plotting as plot
 from Acorn.Analysis.analysis import *
+from array import array
 import argparse
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
@@ -14,7 +15,7 @@ tname = 'WGAnalysis'
 samples = {
     'WGSM': 'output/100718/wgamma_2016_v2/wg_gen/WpAToLNuA0j_5f_LO_MLM_pTA_300_0.root',
     'WGTH': 'output/100718/wgamma_2016_v2/wg_gen/Theorists_0.root',
-    'WG': 'output/100718/wgamma_2016_v2/wg_gen/WpAToLNuA0j_5f_LO_MLM_EFT_pTA_300_0.root'
+    'WG': 'output/100718/wgamma_2016_v2/wg_gen/WGToMuNuG-EFT_pTA_300_inf-madgraphMLM_0.root'
 }
 
 remap = {
@@ -25,6 +26,7 @@ remap = {
 parser = argparse.ArgumentParser()
 parser.add_argument('--draw', default='phi1')
 parser.add_argument('--abs', action='store_true')
+parser.add_argument('--unit-norm', action='store_true')
 parser.add_argument('--charge', default='+1')
 parser.add_argument('--g_pt', default='300.')
 parser.add_argument('--g_eta', default='3.')
@@ -50,28 +52,54 @@ else:
     drawvar = '%s' % args.draw
     binning = (10, -3.15, 3.15)
 
+pt_bins = [300, 400, 600, 800, 1200]
 
 for name, sa, wt in [
-        ('nominal', 'WG', '1.0'),
-        ('C3w_0p1', 'WG', 'weight_C3w_0p1'),
-        ('C3w_0p2', 'WG', 'weight_C3w_0p2'),
-        ('C3w_0p4', 'WG', 'weight_C3w_0p4'),
-        ('C3w_0p8', 'WG', 'weight_C3w_0p8'),
-        ('C3w_1p6', 'WG', 'weight_C3w_1p6'),
-        ('SM',      'WGSM', '1.0'),
-        ('TH',      'WGTH', '1.0')
-        ]:
-    # hists[name] = Hist('TH1D', (10, -3.15, 3.15), 'WG', [drawvar],
-    sel = 'l_charge==%s && nparts>=3 && nparts <=3 && g_pt>%s && l_pt>%s && n_pt>%s && fabs(l_eta) < %s && fabs(n_eta) < %s && fabs(g_eta) < %s && l_g_dr > %s && weight_C3w_0p2 < 10.' % (args.charge, args.g_pt, args.l_pt, args.n_pt, args.l_eta, args.n_eta, args.g_eta, args.dr)
+        ('nominal', 'WG', 'wt_C3w_0p0_'),
+        ('C3w_0p1', 'WG', 'wt_C3w_0p1_'),
+        ('C3w_0p2', 'WG', 'wt_C3w_0p2_'),
+        ('C3w_0p4', 'WG', 'wt_C3w_0p4_'),
+        ('C3w_1p0', 'WG', 'wt_C3w_1p0_'),
+        ('SM', 'WGSM', '1.0'),
+        ('TH', 'WGTH', '1.0')
+]:
+    if args.charge == '0':
+        charge_sel = '1'
+    else:
+        charge_sel = 'l_charge==%s' % args.charge
+    sel = '%s && nparts>=3 && nparts <=10 && g_pt>%s && l_pt>%s && n_pt>%s && fabs(l_eta) < %s && fabs(n_eta) < %s && fabs(g_eta) < %s && l_g_dr > %s' % (
+        charge_sel, args.g_pt, args.l_pt, args.n_pt, args.l_eta, args.n_eta, args.g_eta, args.dr)
     print sel
     hists[name] = Hist('TH1D', binning, sa, [drawvar],
-        # sel='l_charge==+1 && nparts>=3 && nparts <=3 && g_pt>300. && l_pt>80. && n_pt>80. && fabs(l_eta) < 2.4 && l_g_dr > 3.0 && fabs(g_eta) < 3.', wt=wt)
-        sel=sel, wt=wt)
-
+                       sel=sel, wt=wt)
+    hists[name + '_2D'] = Hist('TH2D', binning + (len(pt_bins) - 1, array('d', pt_bins)), sa, [drawvar, 'g_pt'], sel=sel, wt=wt)
 
 MultiDraw(hists, samples, tname)
 
-hists.ForEach(lambda x: NormaliseTo(x, 1.0))
+for ib in xrange(1, hists['nominal_2D'].GetNbinsX() + 1):
+    xmin = hists['nominal_2D'].GetXaxis().GetBinLowEdge(ib)
+    xmax = hists['nominal_2D'].GetXaxis().GetBinUpEdge(ib)
+    for jb in xrange(1, hists['nominal_2D'].GetNbinsY() + 1):
+        ymin = hists['nominal_2D'].GetYaxis().GetBinLowEdge(jb)
+        ymax = hists['nominal_2D'].GetYaxis().GetBinUpEdge(jb)
+        npoints = 5
+        gr = ROOT.TGraph(npoints)
+        nom =  hists['nominal_2D'].GetBinContent(ib, jb)
+        gr.SetPoint(0, 0.0, hists['nominal_2D'].GetBinContent(ib, jb) / nom)
+        gr.SetPoint(1, 0.1, hists['C3w_0p1_2D'].GetBinContent(ib, jb) / nom)
+        gr.SetPoint(2, 0.2, hists['C3w_0p2_2D'].GetBinContent(ib, jb) / nom)
+        gr.SetPoint(3, 0.4, hists['C3w_0p4_2D'].GetBinContent(ib, jb) / nom)
+        gr.SetPoint(4, 1.0, hists['C3w_1p0_2D'].GetBinContent(ib, jb) / nom)
+        canv = ROOT.TCanvas('w_gen_bin_%i_%i' % (ib, jb), 'w_gen_bin_%i_%i' % (ib, jb))
+        pads = plot.OnePad()
+        gr.Draw('APC')
+        gr.Print()
+        plot.DrawTitle(pads[0], '[%g,%g],[%g,%g]' % (xmin, xmax, ymin, ymax), 3)
+        canv.Print('.png')
+        canv.Print('.pdf')
+
+if args.unit_norm:
+    hists.ForEach(lambda x: NormaliseTo(x, 1.0))
 # hists.ForEach(lambda x: WidthDivide(x))
 
 canv = ROOT.TCanvas(args.output, args.output)
@@ -81,14 +109,13 @@ pads = plot.TwoPadSplit(0.27, 0.01, 0.01)
 h_nominal = hists['nominal']
 h_sm = hists['SM']
 h_th = hists['TH']
+h_0p1 = hists['C3w_0p1']
 h_0p2 = hists['C3w_0p2']
 h_0p4 = hists['C3w_0p4']
-h_0p8 = hists['C3w_0p8']
-h_1p6 = hists['C3w_1p6']
+h_1p0 = hists['C3w_1p0']
 
 h_axes = [h_nominal.Clone() for x in pads]
 for h in h_axes:
-    # h.GetXaxis().SetLimits(2.1,200)
     h.Reset()
 
 
@@ -109,25 +136,25 @@ legend = ROOT.TLegend(0.67, 0.86 - 0.04 * 3, 0.90, 0.91, '', 'NBNDC')
 legend.AddEntry(h_nominal, 'EWDim6', 'L')
 legend.AddEntry(h_sm, 'SM', 'L')
 legend.AddEntry(h_th, 'Reference', 'L')
-# legend.AddEntry(h_0p2, 'C_{3W} = 0.2', 'L')
-# legend.AddEntry(h_0p4, 'C_{3W} = 0.4', 'L')
-# legend.AddEntry(h_0p8, 'C_{3W} = 0.8', 'L')
-# legend.AddEntry(h_1p6, 'C_{3W} = 1.6', 'L')
+legend.AddEntry(h_0p1, 'C_{3W} = 0.1', 'L')
+legend.AddEntry(h_0p2, 'C_{3W} = 0.2', 'L')
+legend.AddEntry(h_0p4, 'C_{3W} = 0.4', 'L')
+legend.AddEntry(h_1p0, 'C_{3W} = 1.0', 'L')
 
 plot.Set(h_sm, LineColor=2, LineWidth=2, MarkerColor=2)
 plot.Set(h_th, LineColor=4, LineWidth=2, MarkerColor=4)
-plot.Set(h_0p2, LineColor=8, LineWidth=1)
-plot.Set(h_0p4, LineColor=4, LineWidth=2)
-plot.Set(h_0p8, LineColor=8, LineWidth=2)
-plot.Set(h_1p6, LineColor=9, LineWidth=2)
+plot.Set(h_0p1, LineColor=6, LineWidth=1)
+plot.Set(h_0p2, LineColor=7, LineWidth=1)
+plot.Set(h_0p4, LineColor=9, LineWidth=1)
+plot.Set(h_1p0, LineColor=28, LineWidth=1)
 
 h_nominal.Draw('HISTSAMEE')
-h_sm.Draw('HISTSAMEE')
-h_th.Draw('HISTSAMEE')
-# h_0p2.Draw('HISTSAME')
-# h_0p4.Draw('HISTSAME')
-# h_0p8.Draw('HISTSAME')
-# h_1p6.Draw('HISTSAME')
+# h_sm.Draw('HISTSAMEE')
+# h_th.Draw('HISTSAMEE')
+h_0p1.Draw('HISTSAME')
+h_0p2.Draw('HISTSAME')
+h_0p4.Draw('HISTSAME')
+h_1p0.Draw('HISTSAME')
 
 plot.FixTopRange(pads[0], plot.GetPadYMax(pads[0]), 0.43)
 legend.Draw()
@@ -140,18 +167,18 @@ h_axes[1].Draw()
 
 # r_data = plot.MakeRatioHist(h_data, h_tot, True, False)
 # r_nominal = plot.MakeRatioHist(h_nominal, h_nominal, True, False)
+r_0p1 = plot.MakeRatioHist(h_0p1, h_nominal, True, False)
 r_0p2 = plot.MakeRatioHist(h_0p2, h_nominal, True, False)
 r_0p4 = plot.MakeRatioHist(h_0p4, h_nominal, True, False)
-r_0p8 = plot.MakeRatioHist(h_0p8, h_nominal, True, False)
-r_1p6 = plot.MakeRatioHist(h_1p6, h_nominal, True, False)
-# r_0p2.Draw('HISTSAME')
-# r_0p2.Draw('HISTSAME')
-# r_0p8.Draw('HISTSAME')
-# r_1p6.Draw('HISTSAME')
+r_1p0 = plot.MakeRatioHist(h_1p0, h_nominal, True, False)
+r_0p1.Draw('HISTSAME')
+r_0p2.Draw('HISTSAME')
+r_0p4.Draw('HISTSAME')
+r_1p0.Draw('HISTSAME')
 # r_data.Draw('SAME')
 plot.SetupTwoPadSplitAsRatio(
     pads, plot.GetAxisHist(
-        pads[0]), plot.GetAxisHist(pads[1]), 'C_{3W} = X / Nominal', True, 0.74, 1.26)
+        pads[0]), plot.GetAxisHist(pads[1]), 'C_{3W} = X / Nominal', True, 0.64, 1.36)
 
 
 # Go back and tidy up the axes and frame
@@ -167,11 +194,13 @@ latex = ROOT.TLatex()
 plot.Set(latex, NDC=None, TextFont=42, TextSize=0.03)
 # latex.DrawLatex(0.20, 0.75, args.title)
 plot.DrawTitle(pads[0], '#sqrt{s} = 13 TeV', 3)
-plot.DrawTitle(pads[0], 'W^{%s}#gamma^{} LO - MG5_aMC@NLO 2.4.2' % ('+' if args.charge == '+1' else '-'), 1)
+plot.DrawTitle(pads[0], 'W^{%s}#gamma^{} LO - MG5_aMC@NLO 2.4.2' %
+               ('+' if args.charge == '+1' else '-' if args.charge == '-1' else '#pm'), 1)
 
 pt_l = ROOT.TPaveText(0.23, 0.65, 0.55, 0.9, 'NDCNB')
 pt_l.AddText('Selection: before showering (LHE)')
-pt_l.AddText('p_{T}^{#gamma} > %s GeV, |#eta^{#gamma}| < %s' % (args.g_pt, args.g_eta))
+pt_l.AddText('p_{T}^{#gamma} > %s GeV, |#eta^{#gamma}| < %s' %
+             (args.g_pt, args.g_eta))
 pt_l.AddText('p_{T}^{l} > %s GeV, |#eta^{l}| < %s' % (args.l_pt, args.l_eta))
 pt_l.AddText('p_{T}^{miss} > %s GeV' % args.n_pt)
 pt_l.AddText('#DeltaR(l, #gamma) > %s' % args.dr)
