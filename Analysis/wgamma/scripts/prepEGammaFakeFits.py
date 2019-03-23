bf5e3b78548efdccc2c610213c98271f18b091d5
Andrew Gilbert <andrew.gilbert@cern.ch>
2019-03-23 11:31:40 +0100

    Adding WG NLO stitched configs

--- Analysis/wgamma/scripts/prepEGammaFakeFits.py
+++ Analysis/wgamma/scripts/prepEGammaFakeFits.py
@@ -1,17 +1,27 @@
 import ROOT
-import glob
-import sys
-# import json
 from array import array
 from Acorn.Analysis.analysis import *
+import argparse
 
 ROOT.RooWorkspace.imp = getattr(ROOT.RooWorkspace, 'import')
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
 ROOT.TH1.AddDirectory(0)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-y', '--year', default='2016')
+parser.add_argument('--bkg-sub', action='store_true')
+# parser.add_argument('-o', default='2016', choices=['2016', '2017', '2018'])
+# parser.add_argument('--indir', default='output/130818/wgamma_2016_v2/WGamma/')
+args = parser.parse_args()
+
+sub_bkgs = ['DY_R', 'WG']
+
+if args.bkg_sub:
+    print '>> Subtracting backgrounds %s from data_obs' % str(sub_bkgs)
+
 name = 'EGammaFakes'
 
-fin = ROOT.TFile('output_2016_electron_fakes.root')
+fin = ROOT.TFile('output_%s_electron_fakes.root' % args.year)
 
 pt_bins = [30, 35, 40, 60, 100, 200]
 eta_bins = [0, 1.4442, 2.5]
@@ -21,7 +31,7 @@ hist.GetXaxis().SetTitle('p_pt')
 hist.GetYaxis().SetTitle('abs(p_eta)')
 
 for sample in ['data_obs', 'DY_E']:
-    outfile = ROOT.TFile('ZeeTP_%s.root' % sample, 'RECREATE')
+    outfile = ROOT.TFile('EFakesTP_%s_%s.root' % (args.year, sample), 'RECREATE')
     wsp = ROOT.RooWorkspace('wsp_'+name, '')
     var = wsp.factory('m_ll[100,75,125]')
 
@@ -40,24 +50,17 @@ for sample in ['data_obs', 'DY_E']:
             h_fail = fin.Get('e/fail_%s/l0p0_M/%s' % (dirname, sample))
             h_pass = fin.Get('e/pass_%s/l0p0_M/%s' % (dirname, sample))
 
-            h_pass_bkg = fin.Get('e/pass_%s/l0p0_M/WG' % dirname)
-            h_pass_bkg.Add(fin.Get('e/pass_%s/l0p0_M/DY_R' % dirname))
-            h_fail_bkg = fin.Get('e/fail_%s/l0p0_M/WG' % dirname)
-            h_fail_bkg.Add(fin.Get('e/fail_%s/l0p0_M/DY_R' % dirname))
 
-            if sample == 'data_obs':
+            if args.bkg_sub and sample == 'data_obs':
+                h_pass_bkg = fin.Get('e/pass_%s/l0p0_M/%s' % (dirname, sub_bkgs[0]))
+                h_fail_bkg = fin.Get('e/fail_%s/l0p0_M/%s' % (dirname, sub_bkgs[0]))
+                for bkg in sub_bkgs[1:]:
+                    h_pass_bkg.Add(fin.Get('e/pass_%s/l0p0_M/%s' % (dirname, bkg)))
+                    h_fail_bkg.Add(fin.Get('e/fail_%s/l0p0_M/%s' % (dirname, bkg)))
+
                 h_fail.Add(h_fail_bkg, -1)
                 h_pass.Add(h_pass_bkg, -1)
-    #     for b in cfg['bins']:
-    #         hists[cfg['name']]['%s:fail' % b] = Hist('TH1F', sample=sample, var=[cfg['var']], binning=cfg['binning'], sel='((%s) && !(%s) && (%s))' % (b, cfg['probe'], cfg['tag']), wt='wt_def')
-    #         hists[cfg['name']]['%s:pass' % b] = Hist('TH1F', sample=sample, var=[cfg['var']], binning=cfg['binning'], sel='((%s) && (%s) && (%s))' % (b, cfg['probe'], cfg['tag']), wt='wt_def')
-    #         # drawlist.append((cfg['var'], '((%s) && !(%s) && (%s)) * wt' % (b, cfg['probe'], cfg['tag'])))
-    #         # drawlist.append((cfg['var'], '((%s) && (%s) && (%s)) * wt' % (b, cfg['probe'], cfg['tag'])))
 
-    # MultiDraw(hists, samples, 'WGTagAndProbe', mt_cores=4)
-
-    # # hists = trees[sample].Draw(drawlist, compiled=True)
-   
 
             dat = wsp.imp(ROOT.RooDataHist(binname, '', ROOT.RooArgList(var),
                           ROOT.RooFit.Index(wsp.factory('cat[fail,pass]')),
@@ -69,7 +72,6 @@ for sample in ['data_obs', 'DY_E']:
     # rdh_fail_bkg = ROOT.RooDataHist('rdh_fail_bkg', '', ROOT.RooArgList(var), h_fail_bkg)
 
     # wsp.imp(ROOT.RooHistPdf('backgroundPass', '', ROOT.RooArgSet(var), rdh_pass_bkg))
-    # print h_pass_bkg.Integral()
     # wsp.imp(ROOT.RooHistPdf('backgroundFail', '', ROOT.RooArgSet(var), rdh_fail_bkg))
 
     outfile.cd()
